cee4e359f91d427c0801821355df94edc54a709a
mmmoorman <114218831+mmmoorman@users.noreply.github.com>
2023-07-27 11:09:38 +0200

    type in mom's, removed '

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My mom's new healthy diner')
+streamlit.title('My moms new healthy diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
